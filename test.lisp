58b8fd5d5d3a1a4ad63c2656212bb238089b040c
Vladislav <vladik.ktl@gmail.com>
2017-10-05 16:56:42 +0300

    Update test.lisp

--- test.lisp
+++ test.lisp
@@ -1,5 +1,44 @@
 (load "kolesnikov.lisp")
 
+; helping functions
+
+(defun toString (item)
+  (princ-to-string item) 
+)
+
+(defun _concatAll (lst) 
+  (cond
+    ((null lst) "")
+    ((atom (car lst)) 
+     (concatenate 'string (toString (car lst)) " " (_concatAll (cdr lst))))
+    ((concatenate 'string "(" (_concatAll (car lst)) ") " (_concatAll (cdr lst))))
+  )
+)
+
+(defun concatAll (&rest lst)
+  (_concatAll lst)
+)
+
+; printing result
+
+(defun printValsOfType (var_type props_list)
+  (cond ((null props_list))
+        ((and (print (concatAll var_type (car props_list) " => " (cadr props_list)))
+              (printValsOfType var_type (cddr props_list))
+        ))
+  )
+)
+
+(defun printVals () 
+  (and (printValsOfType 's (SYMBOL-PLIST 's))
+       (printValsOfType 'w (SYMBOL-PLIST 'w))
+       (printValsOfType 'e (SYMBOL-PLIST 'e))
+       (printValsOfType 'v (SYMBOL-PLIST 'v))
+  )
+)
+
+; cleaning stack
+
 (defun _cleanVar (var_type props_list)
   (cond
     ((null props_list))
@@ -21,34 +60,21 @@
   )
 )
 
-(defun printValsOfType (var_type props_list)
-  (cond ((null props_list))
-        ((and (print (concatAll var_type (car props_list) " => " (cadr props_list)))
-              (printValsOfType var_type (cddr props_list))
-        ))
+; testing
+
+(defun testAll (lst)
+  (cond ((null lst)) 
+    ((and (print (concatenate 'string "----------TEMPLATE:" (toString (car lst))))
+          (print (concatenate 'string "----------LIST:    " (toString (cadr lst))))
+          (print (concatenate 'string "RESULT: " (toString (Match (car lst) (cadr lst)))))
+          (printVals)
+          (cleanVars)
+          (testAll (cddr lst))
+    ))
   )
 )
 
-(defun printVals () 
-  (and (printValsOfType 's (SYMBOL-PLIST 's))
-       (printValsOfType 'w (SYMBOL-PLIST 'w))
-       (printValsOfType 'e (SYMBOL-PLIST 'e))
-       (printValsOfType 'v (SYMBOL-PLIST 'v))
-  )
-)
-
-(defun startTesting (lst)
-  (cond ((null lst))
-        ((and 
-            (print (concatenate 'string "----------TEMPLATE:" (toString (car lst))))
-            (print (concatenate 'string "----------LIST:    " (toString (cadr lst))))
-            (or (Match (car lst) (cadr lst)) t)
-            (startTesting (cddr lst))
-        ))
-  )
-)
-
-(startTesting 
+(testAll
   '(
     ((e 1) (s a) (e 1) (s a))       (a b)
 
@@ -70,6 +96,6 @@
 
     ((e 1) ((e 2) (e 3)))           (a (b c))
 
-    (a b c)             (a b)
+    (a b c)                         (a b)
   )
 )
